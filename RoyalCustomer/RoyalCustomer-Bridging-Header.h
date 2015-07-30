3aef716fc6b846330927ac5905bff149964e16ed
Rio Nishida <nr.adiation@gmail.com>
2015-07-30 12:06:05 -0700

    ItemViewController追加

---
+++ RoyalCustomer/RoyalCustomer-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RoyalCustomer-Bridging-Header.h
+//  RoyalCustomer
+//
+//  Created by Rio NISHIDA on 7/30/15.
+//  Copyright (c) 2015 NRI. All rights reserved.
+//
+
+#ifndef RoyalCustomer_RoyalCustomer_Bridging_Header_h
+#define RoyalCustomer_RoyalCustomer_Bridging_Header_h
+
+#import "CorePlot-CocoaTouch.h"
+
+#endif
