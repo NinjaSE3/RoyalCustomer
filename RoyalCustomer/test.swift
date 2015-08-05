a1dcd84baa476503739dd23eda7d150de34a9e47
Your Name <you@example.com>
2015-08-04 23:19:13 -0700

    chg

---
+++ RoyalCustomer/test.swift
@@ -0,0 +1,14 @@
+//
+//  test.swift
+//  RoyalCustomer
+//
+//  Created by Takaaki on 2015/08/04.
+//  Copyright (c) 2015年 NRI. All rights reserved.
+//
+
+import Foundation
+
+var barsData:[(title:String,min:CGFloat,max:CGFloat)] = []
+for(key,value) in pDict{
+        barsData.append((title))
+}
