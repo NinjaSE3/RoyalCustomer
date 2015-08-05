57228edbc81d7769c20dc0bd3a107af1e5c998c0
Your Name <you@example.com>
2015-08-04 21:13:29 -0700

    タブ

--- RoyalCustomer/ItemInfoViewController.swift
+++ RoyalCustomer/ItemInfoViewController.swift
@@ -14,10 +14,6 @@ class ItemInfoViewController: UIViewController{
     var itemViewScrollView = MyScrollView()
     var myWebView : UIWebView!
     
-    var itemImageView:UIImageView!
-    
-    var brandImageView:UIImageView!
-    
     // Viewに関する情報取得
     let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.size.height
     
@@ -33,59 +29,95 @@ class ItemInfoViewController: UIViewController{
     
     override func viewDidLoad() {
         
-        //開発用のため、削除すること
-        clickItem = items[0]
-        println(clickItem)
-        //開発用のため、削除すること
 
         super.viewDidLoad()
         // 背景色をWhiteに設定する.
         self.view.backgroundColor = primaryBackgroundColor
         
-        self.createItemTabView()
-        self.createBrandImageView()
-        self.createItemImageView()
-        self.createWebView()
-        self.createItemViewScrollView()
-        
-        self.view.addSubview(itemTabLeft)
-        self.view.addSubview(itemTabRight)
-        self.view.addSubview(itemViewScrollView)
-        itemViewScrollView.addSubview(itemImageView)
-        itemViewScrollView.addSubview(brandImageView)
-        itemViewScrollView.addSubview(myWebView)
-
+        // itemtabボタン
+        self.itemTabView()
     }
     
-    func createItemTabView(){
+    func itemTabView(){
         itemTabLeft = UIButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.width/2, height: itemTabHight))
         itemTabRight = UIButton(frame: CGRect(x: self.view.frame.width/2, y: 0, width: self.view.frame.width/2, height: itemTabHight))
         
-        itemTabLeft.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
         itemTabLeft.backgroundColor=secondaryBackgroundColor
         itemTabLeft.layer.masksToBounds = true
         itemTabLeft.setTitle("購買履歴" , forState: .Normal)
         itemTabLeft.setTitleColor(primaryColor, forState: .Normal)
         itemTabLeft.titleLabel!.font = UIFont(name: fontName, size: 18)
-        itemTabLeft.layer.position = CGPoint(x: self.itemTabLeft.frame.width/4, y:self.itemTabLeft.frame.height/2)
+        itemTabLeft.layer.position = CGPoint(x: self.itemTabLeft.frame.width/2, y:ViewManager.navigationBarHeight(self) + ViewManager.statusBarHeight + itemTabHight/2)
         itemTabLeft.imageView?.layer.borderColor = secondaryBackgroundColor.CGColor
         itemTabLeft.imageView?.layer.borderWidth = 5
-        
-        itemTabRight.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Right
+        itemTabLeft.addTarget(self, action: "onClickLeftButton:", forControlEvents: .TouchUpInside)
+
+        self.view.addSubview(itemTabLeft)
+
         itemTabRight.backgroundColor=secondaryBackgroundColor
         itemTabRight.layer.masksToBounds = true
-        itemTabRight.setTitle("購買履歴" , forState: .Normal)
+        itemTabRight.setTitle("商品情報" , forState: .Normal)
         itemTabRight.setTitleColor(primaryColor, forState: .Normal)
         itemTabRight.titleLabel!.font = UIFont(name: fontName, size: 18)
-        itemTabRight.layer.position = CGPoint(x: self.itemTabRight.frame.width/4, y:self.itemTabRight.frame.height/2)
+        itemTabRight.layer.position = CGPoint(x: self.itemTabLeft.frame.width + self.itemTabRight.frame.width/2, y:ViewManager.navigationBarHeight(self) + ViewManager.statusBarHeight + itemTabHight/2)
         itemTabRight.imageView?.layer.borderColor = secondaryBackgroundColor.CGColor
         itemTabRight.imageView?.layer.borderWidth = 5
+        itemTabRight.addTarget(self, action: "onClickRightButton:", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(itemTabRight)
+
     }
     
+    func onClickLeftButton(sender: UIButton){
+        println("onClickLeftButton")
+    }
     
-    func createBrandImageView(){
+    func onClickRightButton(sender: UIButton){
+        self.onClickViewDidload()
+    }
+   
+    
+    func onClickViewDidload(){
+        // 背景色をWhiteに設定する.
+        self.view.backgroundColor = primaryBackgroundColor
+        
+//        // itemtabボタン
+//        self.itemTabView()v
+        
+        // 商品詳細View表示用UIScrollView作成
+        itemViewScrollView.frame = CGRectMake(0, itemTabHight, view.bounds.width, view.bounds.height)
+        itemViewScrollView.layer.position = CGPoint(
+            x:view.bounds.width/2,
+            y:view.bounds.height/2 + itemTabHight + ViewManager.statusBarHeight + ViewManager.navigationBarHeight(self)
+        )
+        // タッチイベントを即時取得
+        itemViewScrollView.delaysContentTouches = false
+        itemViewScrollView.backgroundColor = UIColor.clearColor()
+        itemViewScrollView.pagingEnabled = false
+        itemViewScrollView.bounces = true
+        itemViewScrollView.scrollEnabled = true
+        itemViewScrollView.directionalLockEnabled = false
+        itemViewScrollView.showsHorizontalScrollIndicator = true
+        itemViewScrollView.showsVerticalScrollIndicator = false
+        itemViewScrollView.contentSize = CGSizeMake(self.view.bounds.width, self.view.bounds.height * 1.5 - self.navigationController!.navigationBar.bounds.size.height - UIApplication.sharedApplication().statusBarFrame.size.height )
+        self.view.addSubview(itemViewScrollView)
+        
+        
+        //開発用のため、削除すること
+        clickItem = items[0]
+        println(clickItem)
+        //開発用のため、削除すること
+        
+        // 商品画像の表示（ユーザーテスト用）
+        let itemImageView = UIImageView(frame: CGRectMake(0, 0, self.view.bounds.width, itemHeight)) // UIScrollview内での位置設定であることに注意
+        let iteminImage = UIImage(named: clickItem!.image)
+        itemImageView.image = iteminImage
+        itemImageView.contentMode = UIViewContentMode.Center
+        itemImageView.clipsToBounds = true
+        itemViewScrollView.addSubview(itemImageView)
+        
         // ブランドロゴ付与
-        brandImageView = UIImageView(frame: CGRectMake(10, itemHeight - itemHeight/3.0, brandWidth, brandHeight))
+        let brandImageView = UIImageView(frame: CGRectMake(10, itemHeight - itemHeight/3.0 + itemTabHight, brandWidth, brandHeight))
         let brandImage = UIImage(named: clickItem!.brandImage)
         brandImageView.image = brandImage
         brandImageView.layer.cornerRadius = brandImageView.frame.size.width / 2.0
@@ -93,20 +125,10 @@ class ItemInfoViewController: UIViewController{
         brandImageView.layer.borderColor = primaryColor.CGColor
         brandImageView.layer.borderWidth = 2
         brandImageView.contentMode = UIViewContentMode.ScaleAspectFit
-    }
-    
-    func createItemImageView(){
-        // 商品画像の表示（ユーザーテスト用）
-        itemImageView = UIImageView(frame: CGRectMake(0, 0, self.view.bounds.width, itemHeight)) // UIScrollview内での位置設定であることに注意
-        let iteminImage = UIImage(named: clickItem!.image)
-        itemImageView.image = iteminImage
-        itemImageView.contentMode = UIViewContentMode.Center
-        itemImageView.clipsToBounds = true
-    }
-    
-    func createWebView(){
+        itemViewScrollView.addSubview(brandImageView)
+        
         // webViewを生成.
-        myWebView = UIWebView(frame: CGRectMake(0, itemHeight, self.view.bounds.width, self.view.bounds.height * 5 - itemHeight))
+        myWebView = UIWebView(frame: CGRectMake(0, itemHeight + itemTabHight, self.view.bounds.width, self.view.bounds.height - itemHeight ))
         // ファイルパスを生成.
         let path: NSString = NSBundle.mainBundle().pathForResource("webview", ofType:"html")!
         // requestを生成.
@@ -115,31 +137,10 @@ class ItemInfoViewController: UIViewController{
         myWebView.allowsInlineMediaPlayback = false
         // 自動的に再生を開始.
         myWebView.mediaPlaybackRequiresUserAction = false
-        
-        myWebView.scrollView.scrollEnabled = false
         // load開始.
         myWebView.loadRequest(request)
+        itemViewScrollView.addSubview(myWebView)
     }
-    
-    func createItemViewScrollView(){
-        // 商品詳細View表示用UIScrollView作成
-        itemViewScrollView.frame = CGRectMake(0,0, view.bounds.width, view.bounds.height + self.navigationController!.navigationBar.bounds.size.height + UIApplication.sharedApplication().statusBarFrame.size.height - itemTabHight)
-        itemViewScrollView.layer.position = CGPoint(
-            x:view.bounds.width/2,
-            y:itemViewScrollView.frame.height/2 + itemTabHight + self.navigationController!.navigationBar.bounds.size.height + UIApplication.sharedApplication().statusBarFrame.size.height
-        )
-        // タッチイベントを即時取得
-        itemViewScrollView.delaysContentTouches = false
-        itemViewScrollView.backgroundColor = UIColor.redColor()
-        itemViewScrollView.pagingEnabled = false
-        itemViewScrollView.bounces = true
-        itemViewScrollView.scrollEnabled = true
-        itemViewScrollView.directionalLockEnabled = false
-        itemViewScrollView.showsHorizontalScrollIndicator = true
-        itemViewScrollView.showsVerticalScrollIndicator = false
-        itemViewScrollView.contentSize = CGSizeMake(self.view.bounds.width, self.view.bounds.height * 5 - self.navigationController!.navigationBar.bounds.size.height - UIApplication.sharedApplication().statusBarFrame.size.height )
-    }
-    
     
     
     
