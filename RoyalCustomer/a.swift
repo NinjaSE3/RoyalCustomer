48715fe99102a902fff3513d69f7b8632f60d7ce
Your Name <you@example.com>
2015-08-05 20:53:32 -0700

    商品情報コンテンツ拡充

---
+++ RoyalCustomer/a.swift
@@ -0,0 +1,20 @@
+body{
+    background-color: #F8F8F8;
+}
+p {
+    font-family:'HiraKakuProN-W3';
+}
+.bar {
+    background-color: #03A9F4;
+    color: #FFFFFF;
+    padding:0px;
+    margin:0px;
+}
+img{
+    padding:0px;
+    margin:0px;
+}
+center{
+    padding:0px;
+    margin:0px;
+}
